88c1d4e855fd40bf4bb771f3c5b0cb32eccb05c1
Administrator\Nika <nbpberishvili27@gmail.com>
2022-06-28 15:17:35 +0400

    added our services section but not finished.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,18 @@
-const a = 10;
-const b = 15;
-const c = a + b;
-console.log(c);
+const addTocartbutton = document.querySelectorAll(".our-services-section .our-services-containers .button");
+
+console.log(addTocartbutton);
+
+addTocartbutton.forEach(function (item) {
+    item.addEventListener("click", function (event) {
+        const targetElem = event.target;
+
+        const elemTitle = document.querySelectorAll(".our-services-section .our-services-containers .button");
+        if (targetElem.classList.contains("is-active")){
+            targetElem.classList.remove("is-active");
+            elemTitle.innerHTML = "add to cart";
+        }   else {
+            targetElem.classList.contains("is-active");
+            elemTitle.innerHTML = "alredy in cart";
+        }
+    });
+});
